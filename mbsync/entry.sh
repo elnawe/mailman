6d01628b278fef4060e0068b3f68cd8e1764c774
Nahuel Jesús Sacchetti <nahueljsacchetti@gmail.com>
2018-07-01 13:52:54 -0500

    Add server support

--- mbsync/entry.sh
+++ mbsync/entry.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 sed -i "s|<IMAP_USER>|${IMAP_USER}|g" ~/.mbsyncrc
 sed -i "s|<IMAP_PASSWORD>|${IMAP_PASSWORD}|g" ~/.mbsyncrc
+sed -i "s|<IMAP_SERVER>|${IMAP_SERVER}|g" ~/.mbsyncrc
 mbsync -a
